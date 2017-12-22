42b726f34f375f4fc9fc6ff26b2c6005cdf06211
HurricaneTsang <martinszdd@gmail.com>
2017-12-22 10:09:03 +0800

    commit

---
+++ HeartByTime/heart.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	for y := 1.5; y > -1.5; y -= 0.1 {
+
+		//fmt.Println(y)
+		for x := -1.5; x < 1.5; x += 0.05 {
+			//fmt.Println(x)
+			a := x*x + y*y - 1
+			//fmt.Println(a)
+
+			if a*a*a-x*x*y*y*y <= 0.0 {
+				fmt.Printf("*")
+			} else {
+				fmt.Printf("_")
+			}
+
+		}
+
+		time.Sleep(time.Millisecond * 100)
+		fmt.Println(" ")
+
+	}
+}
